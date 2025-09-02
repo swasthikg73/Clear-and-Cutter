fe79b77c7d82527101534a3e9a632e053e295078
swasthikg73 <swasthikg32@gmail.com>
2025-09-02 14:34:34 +0530

    Final Push

---
+++ index.js
@@ -0,0 +1,39 @@
+import fs from "fs/promises";
+import fsn from "fs";
+import path from "path";
+
+let basepath =
+  "E:\\Swasthik\\Sigma Web development\\Backend\\Excercise for sorting of files";
+let files = await fs.readdir(basepath);
+console.log(path);
+
+for (const file of files) {
+  //  let ext = file.split(".")[file.split(".").length - 1];
+
+  let extension = file.split(".").pop();
+
+  if (
+    extension &&
+    !["js", "html", "css", "json"].includes(extension) &&
+    file.split(".").length > 1
+  ) {
+    //Checks is there any directory/ folder available with ext name
+    if (fsn.existsSync(path.join(basepath, extension))) {
+      //Move the file inside the directory or folder
+      await fs.rename(
+        path.join(basepath, file),
+        path.join(basepath, extension, file)
+      );
+    } else {
+      await fs.mkdir(extension);
+      fs.rename(
+        path.join(basepath, file),
+        path.join(basepath, extension, file)
+      );
+    }
+  }
+}
+
+// let a = ["js", "html", "css"];
+// console.log(a.pop());
+// console.log(!a.includes("js"));
